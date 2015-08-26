7e98d366fbf6d6de8c958a22d05141ada81a369c
Alanmugz <alanmugz@gmail.com>
2015-08-26 22:02:59 +0100

    First commit

---
+++ AutoMapper/AutoMapper/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+namespace AutoMapper
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public Address Address { get; set; }
+        public string Position { get; set; }
+        public bool Gender { get; set; }
+        public int Age { get; set; }
+        public int YearsInCompany { get; set; }
+        public DateTime StartDate { get; set; }
+
+        public Employee Build()
+        {
+            return new Employee
+            {
+                Name = "John Smith",
+                Email = "john@email.net",
+                Address = new Address
+                {
+                    Country = "USA",
+                    City = "New York",
+                    Street = "Wall Street",
+                    Number = 7
+                },
+                Position = "Manager",
+                Gender = true,
+                Age = 35,
+                YearsInCompany = 5,
+                StartDate = new DateTime(2007, 11, 2)
+            };
+        }
+    }
+
+    public class Address
+    {
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public int Number { get; set; }
+    }
+
+    public class EmployeeViewItem
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string Position { get; set; }
+        public string Gender { get; set; }
+        public int Age { get; set; }
+        public int YearsInCompany { get; set; }
+        public string StartDate { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Employee = new Employee().Build();
+
+            Mapper.CreateMap<Employee, EmployeeViewItem>()
+               .ForMember(dest => dest.Address,
+                           source => source.MapFrom(employee => employee.Address.City + ", " +
+                                                                employee.Address.Street + " " +
+                                                                employee.Address.Number))
+               .BeforeMap((source, dest) => source.Email = source.Email.ToUpper())
+               .AfterMap((source, dest) => dest.Age = dest.Age + 10)
+               .ForMember(dest => dest.Position, source => source.Condition(src => (src.Position.Equals(""))))
+               .ForMember(dest => dest.Gender,
+                           source => source.ResolveUsing<GenderResolver>().FromMember(e => e.Gender))
+               .ForMember(dest => dest.StartDate, source => source.AddFormatter<DateFormatter>())
+               .ForMember(dest => dest.Name, source => source.Ignore());
+
+            EmployeeViewItem employeeVIewItem = Mapper.Map<Employee, EmployeeViewItem>(Employee);
+
+            Console.Write(employeeVIewItem);
+        }
+
+        public class GenderResolver : ValueResolver<bool, string>
+        {
+            protected override string ResolveCore(bool gender)
+            {
+                return gender ? "Male" : "Female";
+            }
+        }
+
+        public class DateFormatter : IValueFormatter
+        {
+            public string FormatValue(ResolutionContext context)
+            {
+                return ((DateTime)context.SourceValue).ToShortDateString();
+            }
+        }
+    }
+}
---
+++ AutoMapperC/AutoMapperC/Program.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using AutoMapper;
+using NUnit.Framework;
+using Should;
+
+namespace AutoMapperSamples
+{
+    namespace Flattening
+    {
+        public class Order
+        {
+            private readonly IList<OrderLineItem> _orderLineItems = new List<OrderLineItem>();
+
+            public Customer Customer { get; set; }
+
+            public OrderLineItem[] GetOrderLineItems()
+            {
+                return _orderLineItems.ToArray();
+            }
+
+            public void AddOrderLineItem(Product product, int quantity)
+            {
+                _orderLineItems.Add(new OrderLineItem(product, quantity));
+            }
+
+            public decimal GetTotal()
+            {
+                return _orderLineItems.Sum(item => item.GetTotal());
+            }
+        }
+
+        public class Product
+        {
+            public decimal Price { get; set; }
+            public string Name { get; set; }
+        }
+
+        public class OrderLineItem
+        {
+            public OrderLineItem(Product product, int quantity)
+            {
+                Product = product;
+                Quantity = quantity;
+            }
+
+            public Product Product { get; private set; }
+            public int Quantity { get; private set; }
+
+            public decimal GetTotal()
+            {
+                return Quantity * Product.Price;
+            }
+        }
+
+        public class Customer
+        {
+            public string Name { get; set; }
+        }
+
+        public class OrderDto
+        {
+            public string CustomerName { get; set; }
+            public decimal Total { get; set; }
+        }
+
+        [TestFixture]
+        public class Flattening
+        {
+            [Test]
+            public void Example()
+            {
+                // Complex model
+                var customer = new Customer
+                {
+                    Name = "John Doe"
+                };
+                var order = new Order
+                {
+                    Customer = customer
+                };
+                var tv = new Product
+                {
+                    Name = "Sony",
+                    Price = 4.99m
+                };
+                order.AddOrderLineItem(tv, 15);
+
+                // Configure AutoMapper
+                Mapper.CreateMap<Order, OrderDto>();
+
+                // Perform mapping
+                OrderDto dto = Mapper.Map<Order, OrderDto>(order);
+
+                dto.CustomerName.ShouldEqual("John Doe");
+                dto.Total.ShouldEqual(74.85m);
+            }
+
+            static void Main(string[] args)
+            {
+
+            }
+        }
+    }
+}
